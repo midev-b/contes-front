f609be45461c6085eba20bfcde5c1a30135c257c
Mi <belkmi22@gmail.com>
2025-01-31 14:20:21 +0000

    remplacer les import avec des image static

--- src/BOOKSLIST/list.jsx
+++ src/BOOKSLIST/list.jsx
@@ -4,53 +4,31 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
 
 import "./list.css";
-import bookside from "./books/book-side.svg";
+import bookside from "./book-side.svg";
 
-import book1 from "./books/book1.jpg";
-import book2 from "./books/book2.jpg";
-import book3 from "./books/book3.jpg";
-import book4 from "./books/book4.jpg";
-import book5 from "./books/book5.jpg";
-import book6 from "./books/book6.jpg";
-import book7 from "./books/book7.jpg";
-import book8 from "./books/book8.jpg";
-import book9 from "./books/book9.jpg";
-import book10 from "./books/book10.jpg";
-import book11 from "./books/book11.jpg";
-import book12 from "./books/book12.jpg";
-import book13 from "./books/book13.jpg";
-import book14 from "./books/book14.jpg";
-import book15 from "./books/book15.jpg";
-import book16 from "./books/book16.jpg";
-import book17 from "./books/book17.jpg";
-import book18 from "./books/book18.jpg";
-import book19 from "./books/book19.jpg";
-import book20 from "./books/book20.jpg";
-import book21 from "./books/book21.jpg";
-import book22 from "./books/book22.jpg";
 const myBooks = [
-  book1,
-  book2,
-  book3,
-  book4,
-  book5,
-  book6,
-  book7,
-  book8,
-  book9,
-  book10,
-  book11,
-  book12,
-  book13,
-  book14,
-  book15,
-  book16,
-  book17,
-  book18,
-  book19,
-  book20,
-  book21,
-  book22,
+  "/books/book1.jpg",
+  "/books/book2.jpg",
+  "/books/book3.jpg",
+  "/books/book4.jpg",
+  "/books/book5.jpg",
+  "/books/book6.jpg",
+  "/books/book7.jpg",
+  "/books/book8.jpg",
+  "/books/book9.jpg",
+  "/books/book10.jpg",
+  "/books/book11.jpg",
+  "/books/book12.jpg",
+  "/books/book13.jpg",
+  "/books/book14.jpg",
+  "/books/book15.jpg",
+  "/books/book16.jpg",
+  "/books/book17.jpg",
+  "/books/book18.jpg",
+  "/books/book19.jpg",
+  "/books/book20.jpg",
+  "/books/book21.jpg",
+  "/books/book22.jpg",
 ];
 
 export function BOOKSLIST() {
