68af98ab916fdb19550ea5372a311d3908fd39f9
Mi <belkmi22@gmail.com>
2025-09-03 22:06:50 +0100

    checkpoit

--- src/JEUX/GAMESPAGE/RIDDLES/riddles.jsx
+++ src/JEUX/GAMESPAGE/RIDDLES/riddles.jsx
@@ -2,23 +2,21 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { Main } from "../../../PublicComponents/main";
 // import "./games.css";
-import quiz from "/riddles/quiz.jpg";
-import ombres from "/riddles/shadow.jpg";
 
 export function RiddlesPage() {
   return (
-    <div className="games-container">
+    <div className="riddles-container">
       <div className="middle-container">
         <Main />
-        <div className="games-cards">
-          <div className="games-cards-container">
+        <div className="riddles-cards">
+          <div className="riddles-cards-container">
             <Link to="/Jeux/Enigmes/Quiz" className="card quiz-card">
-              <img src={quiz} alt="quiz" />
+              <img src="https://placehold.co/600x400/EEE/31343C" alt="quiz" />
               <h3>QUIZ</h3>
             </Link>
 
             <Link to="/Jeux/Enigmes/Ombres" className="card ombres-card">
-              <img src={ombres} alt="ombres" />
+              <img src="https://placehold.co/600x400/EEE/31343C" alt="ombres" />
               <h3>JEU DES OMBRES</h3>
             </Link>
           </div>
--- src/JEUX/games.jsx
+++ src/JEUX/games.jsx
@@ -2,9 +2,10 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { Main } from "../PublicComponents/main.jsx";
 import "./games.css";
-import riddles from "/games/riddles.png";
-import puzzles from "/games/puzzles.png";
-import memory from "/games/memory.png";
+import riddles from "/GAMES/games/riddles.png";
+import puzzles from "/GAMES/games/puzzles.png";
+import memory from "/GAMES/games/memory.png";
+import kite from "/GAMES/games/kite.png";
 
 export function GamesPage() {
   return (
@@ -12,6 +13,10 @@ export function GamesPage() {
       <div className="middle-container">
         <Main />
         <div className="games-cards">
+          <div className="games-text">
+            <p>Choisis ton jeu préféré et amuse-toi!</p>
+            <img src={kite} alt="cerf-volant" />
+          </div>
           <div className="games-cards-container">
             <Link to="/Jeux/Enigmes" className="card riddles-card">
               <img src={riddles} alt="énigmes" />
